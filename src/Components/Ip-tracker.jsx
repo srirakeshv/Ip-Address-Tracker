80fa1c35888a7dbbf664a9daf292a982e9202226
srirakeshv <srirakeshv@gmail.com>
2024-01-30 19:39:12 +0530

    updated the api slot and map integ

--- src/Components/Ip-tracker.jsx
+++ src/Components/Ip-tracker.jsx
@@ -1,9 +1,71 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Ip-tracker.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 const IpTracker = () => {
+  const [input, setInput] = useState("");
+  const [map, setMap] = useState(null);
+  const [ipInfo, setIpInfo] = useState({
+    ipAddress: "",
+    ipLocation: "",
+    timezone: "",
+    isp: "",
+  });
+  const mapRef = useRef(null);
+
+  const initializeMap = () => {
+    console.log("Initializing map");
+    if (!mapRef.current) {
+      const mapInstance = L.map("map").setView([0, 0], 13);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+        mapInstance
+      );
+      setMap(mapInstance);
+      mapRef.current = mapInstance; // Set the ref to the initialized map
+    }
+  };
+
+  const updateMapMarker = (lat, lng) => {
+    console.log("Updating map marker", lat, lng);
+    if (map && lat !== undefined && lng !== undefined) {
+      map.setView([lat, lng], 13);
+      L.marker([lat, lng]).addTo(map);
+    }
+  };
+
+  useEffect(() => {
+    initializeMap();
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const ipAddress = input;
+    console.log(input);
+    setInput("");
+
+    try {
+      const apiKey = "at_hWTq4MJXWbrFHCaBIte9jqni8bCRF";
+      const apiUrl = `https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=${ipAddress}`;
+
+      const reponse = await fetch(apiUrl);
+      const data = await reponse.json();
+      console.log(data);
+      setIpInfo({
+        ipAddress: data.ip,
+        ipLocation: `${data.location.region}, ${data.location.country}`,
+        timezone: `UTC${data.location.timezone}`,
+        isp: data.isp,
+      });
+      const { lat, lng } = data.location;
+      updateMapMarker(lat, lng);
+    } catch (error) {
+      console.error("Error fetching IP country information:", error);
+    }
+  };
+
   return (
     <div>
       <div
@@ -15,10 +77,12 @@ const IpTracker = () => {
         }}
       >
         <h1>IP Address Tracker</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="Search for any IP address or domain"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
           <button type="submit">
             <FontAwesomeIcon icon={faChevronRight} />
@@ -27,22 +91,23 @@ const IpTracker = () => {
         <div className="informationbar">
           <div>
             <h6>IP ADDRESSES</h6>
-            <p>192.212.174.101</p>
+            <p>{ipInfo.ipAddress || "192.212.174.101"}</p>
           </div>
           <div>
             <h6>LOCATION</h6>
-            <p>Brooklyn, NY 10001</p>
+            <p>{ipInfo.ipLocation || "Brooklyn, NY 10001"}</p>
           </div>
           <div>
             <h6>TIMEZONE</h6>
-            <p>UTC-05:00</p>
+            <p>{ipInfo.timezone || "UTC-05:00"}</p>
           </div>
           <div className="info-last">
             <h6>ISP</h6>
-            <p>SpaceX Starlink</p>
+            <p>{ipInfo.isp || "SpaceX Starlink"}</p>
           </div>
         </div>
       </div>
+      <div id="map" className="leaflet-map"></div>
     </div>
   );
 };
